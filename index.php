7a18b4ee86937ecdf6057a48198b51775fca52de
sergius <nceregao@mail.ru>
2015-08-07 13:27:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="user-scalable=no, width=480"><title>WU.UA 3.0</title><style>a{text-decoration:none;display:block}a:hover{background-color:#eee}i{display:block;margin-left:35px;font-style:normal}</style></head><body>
+<div style="float:left;margin-right:20px"><a target="_blank" href="resources/css/fonts/wu-ico-font-base/demo.html">icon list</a></div>
+<ol style="max-width:800px;min-width:480px;margin:0 auto 200px;font:150%/1.6 Arial">
+
+
+
+<h2>WU.UA 3.0</h2>
+
+
+
+<li><a target="_blank" href="_index.html">Главная</a></li>
+<!-- <li><a target="_blank" href="#">Услуги ----------</a></li> -->
+<!-- <li><a target="_blank" href="#">Проекты ----------</a></li> -->
+<li><a target="_blank" href="contacts.html">Контакты</a></li>
+
+
+
+
+<b>Шаблоны личного кабинета</b>
+<i>
+<li><a target="_blank" href="login.html">Форма входа</a></li>
+<li><a target="_blank" href="account-personal.html">Персональные данные</a></li>
+<li><a target="_blank" href="#">Типовой шаблон кабинета</a></li>
+
+
+<!-- <li><a target="_blank" href="register.html">Регистрация</a></li> -->
+<!-- <li><a target="_blank" href="recovery.html">Восстановление пароля</a></li> -->
+<!-- <li><a target="_blank" href="activation.html">Активация учетной записи</a></li> -->
+</i>
+
+
+
+<!-- <li><a target="_blank" href="news-list.html">Список новостей</a></li> -->
+<!-- <li><a target="_blank" href="document.html">Документ</a></li> -->
+<!-- <li><a target="_blank" href="modal.html">Модальные окна</a></li> -->
+<!-- <li><a target="_blank" href="404.html">404</a></li> -->
+
+
+
+
+
+
+
